b23238be94db5f95d2185a01afb4d11c00b5096c
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2024-04-14 19:34:51 +0100

    vox recog installed & basic test code

--- main.ts
+++ main.ts
@@ -5,6 +5,15 @@ voiceRecognition.setWakeTime(20)
 serial.writeLine("" + (voiceRecognition.getWakeTime()))
 voiceRecognition.playByCMDID(voiceRecognition.checkWord1(voiceRecognition.WakeupWords.W2))
 serial.writeLine("==================")
+basic.showIcon(IconNames.Yes)
 basic.forever(function () {
-	
+    voiceRecognition.getCMDID()
+    if (voiceRecognition.checkCMDID()) {
+        if (voiceRecognition.readCMDID() == voiceRecognition.checkWord3(voiceRecognition.FixedCommandWords.W62)) {
+            basic.showIcon(IconNames.Happy)
+        }
+        if (voiceRecognition.readCMDID() == voiceRecognition.checkWord3(voiceRecognition.FixedCommandWords.W65)) {
+            basic.clearScreen()
+        }
+    }
 })
