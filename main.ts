905ed567d77caceb0fe9c505ca26781b145f1919
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2024-04-14 19:27:27 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+voiceRecognition.init()
+voiceRecognition.setVolume(7)
+voiceRecognition.setMuteMode(voiceRecognition.MUTE.OFF)
+voiceRecognition.setWakeTime(20)
+serial.writeLine("" + (voiceRecognition.getWakeTime()))
+voiceRecognition.playByCMDID(voiceRecognition.checkWord1(voiceRecognition.WakeupWords.W2))
+serial.writeLine("==================")
+basic.forever(function () {
+	
+})
